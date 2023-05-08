16aa455312a5ef8fb5960c0adcd4d59b274360c9
Daniel <denamudu@bariansystems.com>
2023-05-08 20:36:00 +0100

    Added the lendsqr favicon.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
       />
+      <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon" />
       </Head>
       <body>
         <Main />
